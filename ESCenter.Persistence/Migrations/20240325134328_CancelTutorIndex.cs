3a979ebf8dc3fbe66f5dd59a9e0a8e40afd52c52
lehquy13 <lehquy13@gmail.com>
2024-03-26 00:20:28 +0700

    remove indexes

---
+++ ESCenter.Persistence/Migrations/20240325134328_CancelTutorIndex.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ESCenter.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class CancelTutorIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Tutor_AcademicLevel",
+                table: "Tutor");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Tutor_Rate",
+                table: "Tutor");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Tutor_AcademicLevel",
+                table: "Tutor",
+                column: "AcademicLevel");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tutor_Rate",
+                table: "Tutor",
+                column: "Rate");
+        }
+    }
+}
