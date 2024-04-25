ac28b8894649b8eb78d5947606329761c8f82e8f
lehquy13 <lehquy13@gmail.com>
2024-04-25 22:27:10 +0700

    push bb

---
+++ Matt.SharedKernel/Application/Mediators/RequestHandlerBase.cs
@@ -0,0 +1,8 @@
+﻿using Matt.SharedKernel.Domain.Interfaces;
+
+namespace Matt.SharedKernel.Application.Mediators;
+
+public abstract class RequestHandlerBase( IAppLogger<RequestHandlerBase> logger)
+{
+    protected readonly IAppLogger<RequestHandlerBase> Logger = logger;
+}
---
+++ Matt.SharedKernel/Domain/Primitives/Auditing/AuditedAggregateRoot.cs
@@ -0,0 +1,27 @@
+﻿using Matt.Auditing;
+
+namespace Matt.SharedKernel.Domain.Primitives.Auditing;
+
+public abstract class AuditedAggregateRoot<TId> : AggregateRoot<TId>, IAuditedObject
+    where TId : notnull
+{
+    //Creation
+    public DateTime CreationTime { get; protected init; }
+
+    public string? CreatorId { get; protected init; }
+
+    //Modification
+    public DateTime? LastModificationTime { get; protected set;}
+
+    public string? LastModifierId { get; protected set; }
+
+    protected AuditedAggregateRoot(TId id) : base(id)
+    {
+        CreationTime = DateTime.Now.ToLocalTime();
+        LastModificationTime = DateTime.Now.ToLocalTime();
+    }
+
+    protected AuditedAggregateRoot()
+    {
+    }
+}
